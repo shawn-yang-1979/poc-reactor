8748698ba5beaa1e558e8e3e504d43225ac89247
Shawn Yang <ocelot.yang.1979@gmail.com>
2018-02-07 16:58:06 +0800

    first commit

---
+++ notes-on-reactive-programming/src/main/java/idv/shawnyang/pocreactor/PocReactorApplication.java
@@ -0,0 +1,12 @@
+package idv.shawnyang.pocreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocReactorApplication.class, args);
+	}
+}
